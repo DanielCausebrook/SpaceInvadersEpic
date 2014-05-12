e4eac0552fae3feccd22ef25c94f31b1ecc0dd66
Daniel Causebrook <daniel@causebrook.co.uk>
2014-05-12 19:57:39 +0100

    Added Firing alien stub
    This is the stub for adding firing aliens.  It is also a sort-of test
    for branching!

---
+++ SpaceInvadersEPIC/src/spaceInvaders/FiringAlien.java
@@ -0,0 +1,9 @@
+package spaceInvaders;
+
+public class FiringAlien extends AlienEntity{
+    
+    public FiringAlien(Game game,String ref,int x,int y, int row, int col) {
+            super(game,ref,x,y,row,col);
+    }
+    
+}
