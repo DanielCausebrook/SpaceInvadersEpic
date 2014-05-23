a3aec6792d0fc26a3b0ed38e4d9b9b164acf4f18
Daniel300 <daniel@causebrook.co.uk>
2014-05-23 18:52:33 +0100

    Re-made the UpgradeItem class Undid fail...

---
+++ SpaceInvadersEPIC/src/spaceInvaders/UpgradeItem.java
@@ -0,0 +1,24 @@
+package spaceInvaders;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+
+public class UpgradeItem {
+	private int x;
+	private int y;
+	private String text;
+	private Game game;
+	private int cost;
+	private int numUpgradeLevels;
+	private int upgradeLevel;
+	private boolean mouseOver = false;
+
+	public UpgradeItem(Game g, int x, int y, String text,int cost,int numUpgLev) {
+		game=g;
+		this.x=x;
+		this.y=y;
+		this.text=text;
+		this.cost=cost;
+		numUpgradeLevels=numUpgLev;
+	}
+}
