d9d30d7e5a05856ce24c1702d3da4f47e32a00dd
Daniel300 <daniel@causebrook.co.uk>
2014-05-28 18:00:58 +0100

    Finalised

--- SpaceInvadersEPIC/src/spaceInvaders/Game.java
+++ SpaceInvadersEPIC/src/spaceInvaders/Game.java
@@ -86,7 +86,7 @@ public class Game extends Canvas {
         
     private int sparkCount=0;
     
-    private int level = 14;
+    private int level = 0;
     
     private UpgradeShop upgradePanel;
     
